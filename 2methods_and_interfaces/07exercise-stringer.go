67efb4c8693db6d3625eea2f6b271395a6efd478
cowsys <cowsys@gmail.com>
2016-01-09 17:10:26 +0900

    add

---
+++ 2methods_and_interfaces/07exercise-stringer.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+type IPAddr [4]byte
+
+// Stringerインタフェースの実装
+func (i IPAddr) String() string {
+	return fmt.Sprintf("%v, %v, %v, %v", i[0], i[1], i[2], i[3])
+}
+
+func main() {
+	addrs := map[string]IPAddr{
+		"lookback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+
+	for n, a := range addrs {
+		fmt.Printf("%v: %v\n", n, a)
+	}
+}
